67fe592212336000c16c6591f6893290ac7edc66
chenchenfang <50065243+chenchenfang@users.noreply.github.com>
2024-06-25 11:16:34 +0800

    Update jocy_login.js

--- jocy_login.js
+++ jocy_login.js
@@ -2,7 +2,7 @@
 /**
 [rewrite_local]
 # 添加 URL 重写规则，用于匹配需要修改的接口
-http://110.40.42.172:8848/apptov5/v1/user/login?__platform=ios&sign=* script-response-body modify_response.js
+http://110.40.42.172:8848/apptov5/v1/user/login?__platform=ios&sign=* script-response-body https://raw.githubusercontent.com/chenchenfang/img/main/jocy_login.js
 
 [mitm]
 # 确保你已经启用了 MitM 并且信任了 Quantumult X 的证书
