8a664adfa4e46f9964a4c18314e5e560fb8ca713
chenchenfang <50065243+chenchenfang@users.noreply.github.com>
2024-06-25 11:13:02 +0800

    Update jocy_login.js

--- jocy_login.js
+++ jocy_login.js
@@ -1,5 +1,13 @@
 // Quantumult X Script
+/**
+[rewrite_local]
+# 添加 URL 重写规则，用于匹配需要修改的接口
+http://110.40.42.172:8848/apptov5/v1/user/login?__platform=ios&sign=* script-response-body modify_response.js
 
+[mitm]
+# 确保你已经启用了 MitM 并且信任了 Quantumult X 的证书
+hostname = 110.40.42.172
+*/
 const body = $response.body;
 let obj = JSON.parse(body);
 
