896bb81d95d1333b50c5cec27bc95a67a7bf5938
chenchenfang <50065243+chenchenfang@users.noreply.github.com>
2024-06-25 18:20:18 +0800

    Create jocy_getFunctionSwitch.js

---
+++ jocy_getFunctionSwitch.js
@@ -0,0 +1,16 @@
+// Quantumult X Script to modify group_id in the JSON response
+
+/**
+[rewrite_local]
+^http:\/\/110\.40\.42\.172:8848\/apptov5\/v1\/config\/getFunctionSwitch\?.* url script-response-body 
+
+[mitm]
+hostname = 110.40.42.172
+*/
+
+    // Parse the response body
+    let body = JSON.parse($response.body);
+    body.data.group_id = 3
+
+    // Stringify the modified body back to JSON
+    $done({body: JSON.stringify(body)});
