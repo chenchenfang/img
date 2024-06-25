90085007ebe30a055a8f7ac285d38b300b9472f5
chenchenfang <50065243+chenchenfang@users.noreply.github.com>
2024-06-25 11:20:00 +0800

    Update and rename jocy_login_new.js to jocy_login_1.js

--- jocy_login_1.js
+++ jocy_login_1.js
@@ -1,7 +1,7 @@
 // Quantumult X Script
 /**
 [rewrite_local]
-http://110.40.42.172:8848/apptov5/v1/user/login?__platform=ios&sign=* url script-response-body https://raw.githubusercontent.com/chenchenfang/img/main/jocy_login.js
+http://110.40.42.172:8848/apptov5/v1/user/login?__platform=ios&sign=* url script-response-body https://raw.githubusercontent.com/chenchenfang/img/main/jocy_login_1.js
 
 [mitm]
 hostname = 110.40.42.172
